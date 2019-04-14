692296f17a88491a794822d718e55e50d356c6d3
ilsur <subman450@gmail.com>
2019-04-14 21:42:10 +0300

    А вот так вот

---
+++ SadovodClasses/GardenBed.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SadovodClasses
+{
+	public class GardenBed : GardenBedCare								//Класс "грядка"
+	{
+		public class PlantType											//Класс "растения"
+		{
+			private string TypeName;									//Название растения
+			private string SortName;									//Сорт растения
+
+			public PlantType(string TypeName, string SortName)
+			{
+				this.TypeName = TypeName;
+				this.SortName = SortName;
+			}
+
+		}
+
+		public enum GardenBedType										//Перечесление, для указания где расположена грядка		
+		{
+			Alfresco,													//Под открытым небом
+			Hotbed,														//В парнике
+			Greenhouse													//В теплице
+		}
+		private List<PlantType> plants;									//Список раcтений на грядке
+		private string notes;											//Заметки о растениях
+		protected GardenBedType BedType;
+
+		public GardenBed(GardenBedType BedType = GardenBedType.Alfresco)
+		{
+			plants = new List<PlantType>();
+			this.BedType = BedType;
+		}
+
+		public void AddPlant(string TypeName, string SortName)          //Добавление новых растений в грядку
+		{																//Параметры - названия типа и сорта расстения
+			var plantToAdd = new PlantType(TypeName, SortName);
+			plants.Add(plantToAdd);
+		}
+
+		public void RemovePlant(int numberOfPlant)						//Убрать n-ое растение из грядки
+		{
+			plants.RemoveAt(numberOfPlant - 1);
+		}
+
+		public void AddNote(string notesToMake)						   //Сделать заметку о грядке
+		{
+			notes += notesToMake;
+		}
+
+		public void DeleteNote()                            //Очистить заметки о грядке
+		{
+			notes = null;
+		}
+	}
+}
---
+++ SadovodClasses/Stead.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SadovodClasses
+{
+	public class Stead											//Класс "участок"
+	{
+		private List<GardenBed> gardenBeds;						//Поле грядок на участке
+
+		public Stead()
+		{
+			gardenBeds = new List<GardenBed>();
+		}
+
+		public void AddBed(GardenBed gardenBedToToAdd)			//Добавить грядку на участок		
+		{
+			gardenBeds.Add(gardenBedToToAdd);
+		}
+
+		public void RemoveBed(int numberOfBed)					//Убрать n-ую грядку из участка
+		{
+			gardenBeds.RemoveAt(numberOfBed - 1);
+		}
+	}
+}
