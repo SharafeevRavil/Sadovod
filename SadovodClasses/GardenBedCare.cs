c69ef0b7018b5146b31942555172262fec73c109
ilsur <subman450@gmail.com>
2019-04-14 21:27:12 +0300

    А вот так

---
+++ SadovodClasses/GardenBedCare.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace GardenLibrary
+{
+	public class GardenBedCare								//Класс "действия по уходу за грядкой"
+	{
+		//Была ли грядка полита, прополота, окучена, удобрена
+		private bool isPoured, isWeeded, isPiledUp, isFertilized;
+		//Время последнего полива, прополки. окучивания, удобрения
+		private DateTime dateWhenPoured, dateWhenWeeded, dateWhenPiledUp, dateWhenFertilized;
+
+		public void ToPour()                                //Полить грядку, зафиксировать дату и время полива
+		{
+			isPoured = true;
+			dateWhenPoured = DateTime.Now;
+		}
+
+		public void ToWeed()                               //Прополоть грядку, зафиксировать дату и время прополки
+		{
+			isWeeded = true;
+			dateWhenWeeded = DateTime.Now;
+		}
+
+		public void ToPileUp()                            //Окучить грядку, зафиксировать дату и время окучивания
+		{
+			isPiledUp = true;
+			dateWhenPiledUp = DateTime.Now;
+		}
+
+		public void ToFertilize()                        //Удобрить грядку, зафиксировать дату и время удобрения
+		{
+			isFertilized = true;
+			dateWhenFertilized = DateTime.Now;
+		}
+	}
+}
---
+++ SadovodClasses/Stead.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace GardenLibrary
+{
+	public class Stead											//Класс "участок"
+	{
+		private List<GardenBed> gardenBeds;						//Поле грядок на участке
+
+		public Stead()
+		{
+			gardenBeds = new List<GardenBed>();
+		}
+
+		public void AddBed(GardenBed gardenBedToToAdd)			//Добавить грядку на участок		
+		{
+			gardenBeds.Add(gardenBedToToAdd);
+		}
+
+		public void RemoveBed(int numberOfBed)					//Убрать n-ую грядку из участка
+		{
+			gardenBeds.RemoveAt(numberOfBed - 1);
+		}
+	}
+}
