e6088acb3b4bd0357d613233639e05c270accbb5
Alexandr Junushaliev <bazilhan.abdulaev@yandex.ru>
2019-04-15 17:31:41 +0300

    Add JsonExample

--- SadovodBack/Controllers/ExampleConrtoller.cs
+++ SadovodBack/Controllers/ExampleConrtoller.cs
@@ -12,10 +12,65 @@ using SadovodClasses;
 
 namespace WebApplication3.Controllers
 {
+    public class User
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Email { get; set; }
+    }
+
     [Route("api/[controller]")]
     [ApiController]
     public class ExampleController : ControllerBase
     {
+        [Route("ExampleGetJSON/")]
+        [HttpGet]
+        public async Task<JsonResult> ExampleGetJSON()
+        {
+            var user = new User()
+            {
+                Id = "404",
+                Email = "chernikov@gmail.com",
+                UserName = "rollinx",
+                Name = "Andrey",
+                FirstName = "Andrey",
+                MiddleName = "Alexandrovich",
+                LastName = "Chernikov",
+                Gender = "M"
+            };
+            var user1 = new User()
+            {
+                Id = "4",
+                Email = "che",
+                UserName = "r",
+                Name = "An",
+                FirstName = "Ay",
+                MiddleName = "Aledh",
+                LastName = "Cherov",
+                Gender = "F"
+            };
+            var users = new List<User> { user, user1 };
+            var jsonUsers = JsonConvert.SerializeObject(users);
+            return new JsonResult(jsonUsers);
+        }
+        [Route("ExamplePostJSON/")]
+        [HttpPost]
+        public async void ExamplePutJSON(int id, [FromBody] string value)
+        {
+            var users = JsonConvert.DeserializeObject<List<User>>(value);
+        }
         [Route("GetAll/")]
         [HttpGet]
         public async Task<JsonResult> Get()
