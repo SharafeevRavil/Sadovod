6b596dcfab656795557efb6f778e07df636160f8
ilsur <subman450@gmail.com>
2019-04-23 18:46:07 +0300

    s

---
+++ Identity/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Identity.ViewModels
+{
+	public class RegisterViewModel
+	{
+		public string Email { get; set; }
+		public string Password { get; set; }
+		public string ConfirmPassword { get; set; }
+
+		public ICommand RegisterCommand
+		{
+			get
+			{
+				return new Command(() =>
+				{
+
+				});
+			}
+		}
+	}
+}
